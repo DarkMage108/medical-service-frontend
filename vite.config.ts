dbf47f9b9e9695948291fa4b4f73af07486d6e67
DarkMage108 <alihanbaba108@gmail.com>
2025-12-10 13:00:41 +0000

    feat: update allow frontend

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', 'VITE_');
+    const env = loadEnv(mode, '..', 'VITE_');
     return {
       server: {
         port: 3000,
         host: '0.0.0.0',
+        allowedHosts: [env.VITE_ALLOWED_HOST || 'localhost'],
         proxy: {
           '/api': {
             target: env.VITE_API_URL?.replace('/api', '') || 'http://localhost:3002',
